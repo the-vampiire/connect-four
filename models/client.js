3128a00f552a4ff1c9cb30b2eddad04ae2147490
The-Vampiire <vampiirecodes@gmail.com>
2018-06-23 00:42:37 -0400

    updated Client to interact with refactored GameAnalyzer

--- models/client.js
+++ models/client.js
@@ -1,20 +1,17 @@
 class Client {
     constructor(game) {
         this.game = game;
-        this.createBoard();
-    }
 
-    setup() {
         const resetButton = document.querySelector('#reset');
-        resetButton.addEventListener('click', () => this.game.reset());
+        resetButton.addEventListener('click', () => this.game.reset()); 
     }
 
-    createBoard() {
+    drawBoard() {
         const boardElement = document.querySelector('#board');
         this.game.board.forEach(column => boardElement.append(column.div));
     }
 
-    displayWinner(winner) {
-        // console.log(winner);
+    declareWinner(winnerData) {
+        console.log(winnerData);
     }
 }
