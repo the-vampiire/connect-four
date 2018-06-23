3bf849800b70424e45cf7bb03ce0494e6b37ed11
The-Vampiire <vampiirecodes@gmail.com>
2018-06-23 00:43:55 -0400

    added Player creation mechanism in main()

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
+// const getPlayerData = () => {
+//     // selects the User input fields and maps them to collect their values
+//     const values = Array.from(document.querySelectorAll(
+//         '#playerOneName, #playerOneColor, #playerTwoName, #playerTwoColor'
+//     ), element => element.value);
+
+//     return {
+//         playerOneData: { name: values[0], color: values[1] },
+//         playerTwoData: { name: values[2], color: values[3] }
+//     };
+// }
+
+// const createPlayers = () => {
+//     const { playerOneData, playerTwoData } = getPlayerData();
+
+//     return {
+//         playerOne: new Player(playerOneData.name, playerOneData.color),
+//         playerTwo: new Player(playerTwoData.name, playerTwoData.color),
+//     };
+// }
+
 window.addEventListener('load', () => {
+    // const { playerOne, playerTwo } = createPlayers();
+    // const game = new ConnectFour(playerOne, playerTwo);
     const game = new ConnectFour();
     game.start();
 });
