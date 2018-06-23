2a21cf0b48feef7a1d7f1724ef2151a5293a1ed0
The-Vampiire <vampiirecodes@gmail.com>
2018-06-22 21:00:22 -0400

    Refactored Column to use association with ConnectFour governing object

--- models/column.js
+++ models/column.js
@@ -1,14 +1,13 @@
-export default class Column {
-    constructor(client, index) {
-        this.client = client;
+class Column {
+    constructor(index, game) {
         this.index = index;
-        this.cells = [];
-        this.div = this.generateDiv();
+        this.game = game;
 
-        this.populateCells();
+        this.cells = []; // holds the cells for 'this' Column
+        this.div = this.createDiv(); // column 'div' for Client use
     }
 
-    generateDiv() {
+    createDiv() {
         const col = document.createElement('div');
         col.className = 'column';
         col.setAttribute('column', this.index);
@@ -17,29 +16,29 @@ export default class Column {
         return col;
     }
 
-    populateCells() {
+    populateCells() { // populates the column with Cell objects
         for(let row = 0; row < 6; ++row) {
-            const cell = new Cell(this, row);
+            const cell = new Cell(this.index, row);
             this.cells.push(cell);
-            this.div.append(cell.div);
+            this.div.append(cell.div); // populate the Cell divs into the Column div
         }
     }
 
-    dropPiece() {
+    emptyCells() { // empties all cells in 'this' Column
+        this.cells.forEach(cell => cell.empty());
+    }
+
+    dropPiece() { // finds the next lowest Cell in 'this' Column and "drops" a piece 
         let row = this.cells.length - 1;
         let cell = this.cells[row];
 
-        while (cell.playerPiece !== null) {
-            if (row === 0) return; // prevent pieces from overlapping
-            row--;
+        while (cell.piece !== null) { // decrements until finding an available Cell
+            if (row === 0) return; // prevent pieces from overlapping at the top
+            --row;
             cell = this.cells[row];
         }
-
-        cell.occupy(this.client.currentPlayer);
-        this.client.game.placePiece(cell.column.index, cell.row);
-    }
-
-    emptyCells() {
-        this.cells.forEach(cell => cell.empty());
+        
+        cell.occupy(this.game.currentPlayer); // occupies the available Cell with the Player's color
+        this.game.update();
     }
 }
